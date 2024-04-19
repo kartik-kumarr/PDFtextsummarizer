0399157dd222d5b34694eec1fd06428b2978d1b5
Kartik <kartik@Kartiks-Air.bbrouter>
2024-04-19 17:43:27 +0100

    Updated Navbar

--- src/client/Navbar.jsx
+++ src/client/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <header className="mt-3 mb-auto">
       <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-center"> {/* Changed justify-content-lg-start to justify-content-lg-center */}
-        <h3 className="mb-0">Comparing LLms</h3> {/* Removed extra space before "mb-0" */}
+        <h3 className="mb-0">Comparing LLMs</h3> 
       </div>
     </header>
   );
